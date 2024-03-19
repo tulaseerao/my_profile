70e7099887dad4bc1176b783197d56e851646314
PoonamYanthraa <poonam.b@yanthraa.com>
2024-03-19 10:27:19 +0530

    Title update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Tulasee Rao",
+  title: "Tulasee Rao Chintha | Inpharmd, CTO | YC W21 | AI Specialist",
   description: "My Profile",
 };
 
